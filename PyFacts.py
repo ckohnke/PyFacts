5b80fc4f89dad4b2c189ec7fecbd83ac05a58dcc
Colton Kohnke <colton.kohnke@gmail.com>
2014-02-10 05:43:20 +0000

    Added feature to be subscribed to multiple lists.

--- PyFacts.py
+++ PyFacts.py
@@ -6,29 +6,42 @@ import time
 # import voice/lists
 voice = Voice()
 voice.login()
-with open("number.db") as f:
-    nums = f.readlines()
+with open("num_cat.db") as f:
+    nums_cat = f.readlines()
+with open("num_pok.db") as f:
+    nums_pok = f.readlines()
+with open("cat.db") as f:
+    fax_cat = f.readlines()
 with open("pokemon.db") as f:
     fax_pok = f.readlines()
 
 # Send the list a random fact each
 def daily():
-  for i in range(0,len(nums)-1):
-    voice.send_sms(nums[i],fax_pok[randint(0,len(fax_pok)-1)])
+  for i in range(0,len(nums_cat)-1):
+    voice.send_sms(nums_cat[i],fax_cat[randint(0,len(fax_cat)-1)])
+  for i in range(0,len(nums_pok)-1):
+    voice.send_sms(nums_pok[i],fax_pok[randint(0,len(fax_pok)-1)])
 
 # Check inbox, respond to unread messages
 def checkInbox():
-  for message in voice.inbox().messages:
+  for message in voice.sms().messages:
       if not message.isRead:
           n = message.phoneNumber
           message.mark()
-          voice.send_sms(n,fax_pok[randint(0,len(fax_pok)-1)])
+          if str(n) in str(nums_cat):
+            voice.send_sms(n,fax_cat[randint(0,len(fax_cat)-1)])
+          if str(n) in str(nums_pok):
+            voice.send_sms(n,fax_pok[randint(0,len(fax_pok)-1)])
+          if not str(n) in str(nums_cat) and not str(n) in str(nums_pok):
+            voice.send_sms(n,fax_cat[randint(0,len(fax_cat)-1)])
+          time.sleep(5)
 
 def main():
-  perdiem = 5 # facts/day for daily
+  perdiem = 3 # facts/day for daily
   start = 86400/perdiem
   sinDay = start
   s = 5 # Sleep incrimentor (s)
+  #daily()
   while True:
     if sinDay < 0:
       daily()  # Send fact perdiem/day to number list
