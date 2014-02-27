0dc2d707d9b236eb58c8331bec310efef15c6fef
Colton Kohnke <colton.kohnke@gmail.com>
2014-02-27 06:30:56 +0000

    added more

--- PyFacts.py
+++ PyFacts.py
@@ -10,17 +10,33 @@ with open("num_cat.db") as f:
     nums_cat = f.readlines()
 with open("num_pok.db") as f:
     nums_pok = f.readlines()
+with open("num_gir.db") as f:
+    nums_gir = f.readlines()
+with open("num_sex.db") as f:
+    nums_sex = f.readlines()
 with open("cat.db") as f:
     fax_cat = f.readlines()
 with open("pokemon.db") as f:
     fax_pok = f.readlines()
+with open("giraffe.db") as f:
+    fax_gir = f.readlines()
+with open("sex.db") as f:
+    fax_sex = f.readlines()
 
 # Send the list a random fact each
 def daily():
   for i in range(0,len(nums_cat)-1):
     voice.send_sms(nums_cat[i],fax_cat[randint(0,len(fax_cat)-1)])
+    time.sleep(10)
   for i in range(0,len(nums_pok)-1):
     voice.send_sms(nums_pok[i],fax_pok[randint(0,len(fax_pok)-1)])
+    time.sleep(10)
+  for i in range(0,len(nums_gir)-1):
+    voice.send_sms(nums_gir[i],fax_gir[randint(0,len(fax_gir)-1)])
+    time.sleep(10)
+  for i in range(0,len(nums_sex)-1):
+    voice.send_sms(nums_sex[i],fax_sex[randint(0,len(fax_sex)-1)])
+    time.sleep(10)
 
 # Check inbox, respond to unread messages
 def checkInbox():
@@ -28,22 +44,26 @@ def checkInbox():
       if not message.isRead:
           n = message.phoneNumber
           message.mark()
-          if str(n) in str(nums_cat):
+          if str(n) in str(nums_cat): # if in cat list
             voice.send_sms(n,fax_cat[randint(0,len(fax_cat)-1)])
-          if str(n) in str(nums_pok):
+          if str(n) in str(nums_pok): # if in pokemon list
             voice.send_sms(n,fax_pok[randint(0,len(fax_pok)-1)])
-          if not str(n) in str(nums_cat) and not str(n) in str(nums_pok):
+          if str(n) in str(nums_gir): # if in giraffe list
+            voice.send_sms(n,fax_gir[randint(0,len(fax_gir)-1)])
+          if str(n) in str(nums_sex): # if in giraffe list
+            voice.send_sms(n,fax_sex[randint(0,len(fax_sex)-1)])
+          if not str(n) in str(nums_cat) and not str(n) in str(nums_pok) and not str(n) in str(nums_gir) and not str(n) in str(nums_sex): # if in no list, default to cat
             voice.send_sms(n,fax_cat[randint(0,len(fax_cat)-1)])
-          time.sleep(5)
+          time.sleep(10)
 
 def main():
-  perdiem = 3 # facts/day for daily
-  start = 86400/perdiem
+  perdiem = 6.0 # facts/day for daily
+  start = 86400.0/perdiem
   sinDay = start
-  s = 5 # Sleep incrimentor (s)
-  #daily()
+  s = 10 # Sleep incrimentor (s)
+  daily()
   while True:
-    if sinDay < 0:
+    if sinDay < 100:
       daily()  # Send fact perdiem/day to number list
       sinDay = start
     time.sleep(s)
